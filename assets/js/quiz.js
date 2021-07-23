55a3adabb9e57cd025eea7ab83f7464e4a4b084d
dannymagnus <mrdanrichards@gmail.com>
2021-07-23 17:50:56 +0100

    js: add randomiser function: bugs exist for displaying next question

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -12,12 +12,49 @@ toggleButton.addEventListener('click', () => {
 let beginButton = document.getElementById('begin-btn');
 beginButton.addEventListener('click', startQuiz);
 let quesContainer = document.getElementById('ques-container');
+let question = document.getElementById('question');
+let answers = document.getElementById('answers');
+
+// Declare  variables to be used for random question generation in function//
+let shuffledQuestions = new Array;
+let currentQuestionIndex;
+
+let questions = [
+  {question: 'What was the name of the computer simulation beaten by James T. Kirk?',
+  answer:[
+  {option:'Kobiyashi Maru', correct:true},
+  {option:'Kobiyashi Teng', correct:false},
+  {option:'Warbird Krath', correct:false},
+  {option:'Warbird Warf', correct:false}]}
+];
 
 // Function to remove and add start and question box //
+
 function startQuiz(){
-console.log("quiz-started");
-beginButton.classList.add('hidden');
-quesContainer.classList.remove('hidden');
+  console.log("quiz-started");
+  beginButton.classList.add('hidden');
+  quesContainer.classList.remove('hidden');
+  shuffle();
+  console.log(shuffledQuestions[0].answer[0].option)
+  currentQuestionIndex = 0;
+  nextQuestion();
 }
 
+function nextQuestion(){
+  revealQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
+function revealQuestion(question){
+  question.innerText = question.question;
+}
+//Question Randomiser //
+
+function shuffle() {
+  let cloneQuestions = JSON.parse(JSON.stringify(questions));
+
+  for (let i=0; i < cloneQuestions.length ; i++){
+      let randomIndex = Math.floor(Math.random() * cloneQuestions.length);
+      shuffledQuestions[i] = cloneQuestions[randomIndex];
+      cloneQuestions.splice(randomIndex,1);
+  }
+}
