0146bd6bbb4c4ee51328ccf0e5f94bc694767656
dannymagnus <mrdanrichards@gmail.com>
2021-08-02 16:50:09 +0100

    css: fix update woff2 font face

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,21 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
+//for contact form button
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      emailjs.init("user_mrJgfpy8vz9l8LqeGGrjA");
+      // these IDs from the previous steps
+      emailjs.sendForm('service_y7rgdam', 'dans_template', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
 
 //css rule for nav button highlight//
 
