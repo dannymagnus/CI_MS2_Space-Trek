d9e56ee771b93d7f538ac59d9c05ae97a5d67235
dannymagnus <mrdanrichards@gmail.com>
2021-08-03 12:12:39 +0100

    html: copy footer to quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,7 +6,6 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
-
 //Variable declarations for quiz page //
 // Variable to grab quiz form
 let quizRegister = document.getElementById('quiz-register');
@@ -119,6 +118,7 @@ function startQuiz(){
   enemyShields = 100;
   yourShieldPercent.innerText = yourShields;
   enemyShieldPercent.innerText = enemyShields;
+  
 }
 
 function nextQuestion(){
@@ -373,3 +373,73 @@ let questions = [
   {option:'Spock', correct:false},
   {option:'McCoy', correct:false}]}
 ];
+
+
+//To add launch button for own modal
+const contactLaunch = document.querySelector('#contact-launch');
+//To add listener for own modal
+const contactFormOuter = document.querySelector('#contact-form-outer');
+//To grab main divs for sucess message fade in and out
+const mainContent = document.getElementById('main-content');
+// Grab to add and remove function created child
+const mainContainer = document.getElementById('main-container');
+//To use variable with Jquery to hide and reveal main content
+const mainContentId = mainContent.getAttribute('id');
+
+
+//Toggle to reveal self made modal
+contactLaunch.addEventListener('click', () => {
+  $("#contact-form-outer").fadeIn(1000);
+  contactFormOuter.style.display = 'flex';
+})
+
+//for contact form button and use mailjs API
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    // prevent default action
+    event.preventDefault();
+    emailjs.init("user_mrJgfpy8vz9l8LqeGGrjA");
+    // these IDs from the previous steps
+    emailjs.sendForm('service_y7rgdam', 'dans_template', this)
+      .then(function () {
+        console.log('SUCCESS!');
+      }, function (error) {
+        console.log('FAILED...', error);
+      });
+    //On complete fade out modal
+    $("#contact-form-outer").fadeOut(1000);
+    //Call function display success message pass variables from grab at page top
+    createSuccessMessage(mainContainer, mainContent);
+    //Function to create success message and append to body
+    function createSuccessMessage(outer, inner) {
+      var contactSuccessElement = document.createElement('div');
+      contactSuccessElement.setAttribute('id', 'contact-success');
+      contactSuccessElement.style.cssText = "display:none; border: 1px solid darkblue; background-color: rgba(var(--hue-neutral),.5); padding:30px; border-radius:30px; text-align:center;"
+      var contactSuccessHTML =
+        `
+  <div>
+          <h2>Reply from Space Fleet</h2>
+          <p>...message recieved.  We'll get right on it.  Stay your course Captain.</p>
+          <button id = "ack-btn" class = "btn contact-btn ack">Ok</button>
+  </div>
+  `;
+      contactSuccessElement.innerHTML = contactSuccessHTML;
+      outer.appendChild(contactSuccessElement);
+      inner.style.display = 'none';
+      setTimeout(function () {
+        $('#contact-success').fadeIn(1000);
+        //your code to be executed after 1 second
+      }, 1000);
+      // Add event listener to acknowledge button to fade and remove div and return previous
+      const ackBtn = document.querySelector('#ack-btn');
+      ackBtn.addEventListener('click', () => {
+        $('#contact-success').fadeOut(1000);
+        const contactSuccessElement = document.querySelector('#contact-success');
+        setTimeout(function () {
+          mainContainer.removeChild(contactSuccessElement);
+          $('#' + mainContentId).fadeIn(1000);
+        }, 1000);
+      });
+    }
+  });
+}
