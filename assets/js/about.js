eb6e806e92723a1feb3776264ec640b880eb1e74
dannymagnus <mrdanrichards@gmail.com>
2021-08-07 12:38:01 +0100

    js: complete planet object

--- assets/js/about.js
+++ assets/js/about.js
@@ -125,3 +125,88 @@ function figureModalContent(planet) {
       break;
   }
 }
+
+let planetArray = [
+  {
+    name:'Sun',
+    summary: 
+    `
+    <p>Our Sun – the heart of our solar system – is a yellow dwarf star, a hot ball of glowing gases. Its gravity holds the solar system together, keeping everything from the biggest planets to the smallest particles of debris in its orbit. Electric currents in the Sun generate a magnetic field that is carried out through the solar system by the solar wind – a stream of electrically charged gas blowing outward from the Sun in all directions.</p>
+    <p>The Sun is the largest object in our solar system, comprising 99.8% of the system’s mass. Though it seems huge to us, the Sun isn't as large as other types of stars.</p>
+    <p>Earth orbits the Sun from a distance of about 93 million miles. The connection and interactions between the Sun and Earth drive our planet's seasons, ocean currents, weather, climate, radiation belts, and aurorae. Though it is special to us, there are billions of stars like our Sun scattered across the Milky Way galaxy.</p>
+    `
+  },
+  {
+    name: 'Mercury',
+    summary: 
+    `
+    <p>The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earth's Moon.</p>
+    <p>From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as seven times brighter. Despite its proximity to the Sun, Mercury is not the hottest planet in our solar system – that title belongs to nearby Venus, thanks to its dense atmosphere.</p>
+    `
+  },
+  {
+    name:'Venus',
+    summary:
+    `
+    <p>Venus is the second planet from the Sun and is Earth’s closest planetary neighbor. It’s one of the four inner, terrestrial (or rocky) planets, and it’s often called Earth’s twin because it’s similar in size and density. These are not identical twins, however – there are radical differences between the two worlds.</p>
+    <p>Venus has a thick, toxic atmosphere filled with carbon dioxide and it’s perpetually shrouded in thick, yellowish clouds of sulfuric acid that trap heat, causing a runaway greenhouse effect. It’s the hottest planet in our solar system, even though Mercury is closer to the Sun. Surface temperatures on Venus are about 900 degrees Fahrenheit (475 degrees Celsius) – hot enough to melt lead. The surface is a rusty color and it’s peppered with intensely crunched mountains and thousands of large volcanoes. Scientists think it’s possible some volcanoes are still active.
+
+    Venus has crushing air pressure at its surface – more than 90 times that of Earth – similar to the pressure you'd encounter a mile below the ocean on Earth.
+    
+    Another big difference from Earth – Venus rotates on its axis backward, compared to most of the other planets in the solar system. This means that, on Venus, the Sun rises in the west and sets in the east, opposite to what we experience on Earth.</p>
+    `
+  },
+  {
+    name:'Earth',
+    summary: 
+    `
+    <p>Our home planet is the third planet from the Sun, and the only place we know of so far that’s inhabited by living things.</p>
+    <p>While Earth is only the fifth largest planet in the solar system, it is the only world in our solar system with liquid water on the surface. Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal.
+
+    The name Earth is at least 1,000 years old. All of the planets, except for Earth, were named after Greek and Roman gods and goddesses. However, the name Earth is a Germanic word, which simply means “the ground.”</p>
+    `
+  },
+  {
+    name:'Mars',
+    summary:
+    `
+    <p>Mars is the fourth planet from the Sun – a dusty, cold, desert world with a very thin atmosphere. Mars is also a dynamic planet with seasons, polar ice caps, canyons, extinct volcanoes, and evidence that it was even more active in the past.</p>
+    <p>Mars is one of the most explored bodies in our solar system, and it's the only planet where we've sent rovers to roam the alien landscape.</p>
+    `
+  },
+  {
+    name:'Jupiter',
+    summary:
+    `
+    <p>Saturn is the sixth planet from the Sun and the second-largest planet in our solar system.</p>
+    <p>Adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings – made of chunks of ice and rock – but none are as spectacular or as complicated as Saturn's.
+
+    Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium.</p>
+    `
+  },
+  {
+    name:'Saturn',
+    summary:
+    `
+    <p>Saturn is the sixth planet from the Sun and the second-largest planet in our solar system.</p>
+    <p>Adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings – made of chunks of ice and rock – but none are as spectacular or as complicated as Saturn's.
+
+    Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium.</p>
+    `
+  },
+  {
+    name:'Uranus',
+    summary:
+    `
+    <p>Uranus is the seventh planet from the Sun, and has the third-largest diameter in our solar system. It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star.</p>
+    <p>It was two years later that the object was universally accepted as a new planet, in part because of observations by astronomer Johann Elert Bode. Herschel tried unsuccessfully to name his discovery Georgium Sidus after King George III. Instead, the scientific community accepted Bode's suggestion to name it Uranus, the Greek god of the sky, as suggested by Bode.​</p>
+    `
+  },
+  {
+    name:'Neptune',
+    summary:
+    `
+    <p>Dark, cold, and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system.</p>
+    <p>More than 30 times as far from the Sun as Earth, Neptune is the only planet in our solar system not visible to the naked eye and the first predicted by mathematics before its discovery. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846.</p>
+    `
+  }];
