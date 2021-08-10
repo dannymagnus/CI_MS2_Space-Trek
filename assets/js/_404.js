4d89ceb5d8075db16a02c99c3a987f02d4adc9f8
dannymagnus <mrdanrichards@gmail.com>
2021-08-10 21:31:38 +0100

    js: update js comments

--- assets/js/_404.js
+++ assets/js/_404.js
@@ -2,8 +2,10 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
-//Toggle for main nav bar
+/* 
+* Event listener with function to toggle responsive navbar burger icon
+*/
 toggleButton.addEventListener('click', () => {
-    "use strict";
-    navbarLinks.classList.toggle('active');
-  });
+  "use strict";
+  navbarLinks.classList.toggle('active');
+});
