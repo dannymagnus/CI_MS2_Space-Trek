c3ae7b8a9f6a7b28e2ed92fe6bbe1652a5d2c58e
dannymagnus <mrdanrichards@gmail.com>
2021-08-11 13:53:26 +0100

    docs: update readme

--- assets/js/about.js
+++ assets/js/about.js
@@ -13,14 +13,14 @@ const mainContainer = document.getElementById('about-main-container');
 const mainContentId = mainContent.getAttribute('id');
 const modalClose = document.querySelector('#modal-close');
 
-/* 
+/**
 * Event listener with function to toggle responsive navbar burger icon
 */
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
 
-/*
+/**
 * Event listener with function to reveal self made modal of contact form
 */
 contactLaunch.addEventListener('click', () => {
@@ -28,7 +28,7 @@ contactLaunch.addEventListener('click', () => {
   contactFormOuter.style.display = 'flex';
 });
 
-/*
+/**
 * Function to prevent default form submission and send completed form values to email credit www.mailjs.com
 */
 window.onload = function () {
@@ -65,7 +65,7 @@ window.onload = function () {
     $("#contact-form-outer").fadeOut(1000);
     createSuccessMessage(mainContainer, mainContent);
 
-    /*
+    /**
     * Function to display success message
     * It creates a div element
     * It sets attributes and styles to the div element
@@ -89,7 +89,7 @@ window.onload = function () {
       contactSuccessElement.innerHTML = contactSuccessHTML;
       outer.appendChild(contactSuccessElement);
       inner.style.display = 'none';
-      /*
+      /**
       * Delay to fade in message sent confirmation
       * Fades in the confirmation message after a delay of 1 second
       */
@@ -97,7 +97,7 @@ window.onload = function () {
         $('#contact-success').fadeIn(1000);
       }, 1000);
       const ackBtn = document.querySelector('#ack-btn');
-      /* 
+      /**
       * Event listener with function to close form submission success message and return main page content
       * It fades out the contact success message then fades in the main content after delay
       */
@@ -113,7 +113,7 @@ window.onload = function () {
   });
 };
 
-/*
+/**
 * Event lister with arrow function to fade contact form modal and return previous content if close button clicked
 */
 modalClose.addEventListener('click', () => {
